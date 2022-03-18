bc39f2c5dfa203c35a774abb3675254c909a0bba
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2022-03-18 14:07:59 +0100

    add reviews project (03)

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import Review from './Review';
+// import { FaGithubSquare } from 'react-icons/fa';
+
+/* function App() {
+  return (
+    <h2>
+      reviews project setup
+      <FaGithubSquare className='icon' />
+    </h2>
+  );
+} */
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
 }
 
 export default App;
